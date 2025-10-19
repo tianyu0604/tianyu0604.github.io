fcb74d88b40de0747fa4bcba309044c13367c7b5
tianyu0604 <tianyu0604@users.noreply.github.com>
2025-10-19 11:57:15 +0000

    deploy: c4722f971dba85ba4ef776bd4299106e20046db8

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760873452',resources: [
+    cacheName: 'chirpy-1760875034',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
